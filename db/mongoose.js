56a7b21238cba8bbd734972376b3f7c8c98bf34f
Junyuan Li <junyuan.li@outlook.com>
2022-09-11 19:17:07 -0400

    put connection string in Heroku web UI

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 /* Connnect to our database */
 // Get the URI of the local database, or the one specified on deployment.
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://lijunyua:lijunyua@rateyourcoursescluster.yhbldxc.mongodb.net/RateYourCoursesDB?retryWrites=true&w=majority'
+const mongoURI = process.env.MONGODB_URI || process.env.MONGODB_CONNECTION_STRING
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 .catch((error) => {
